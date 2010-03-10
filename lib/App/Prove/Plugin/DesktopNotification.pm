066fbfc98e6254dee00789004abd81801d102ce5
sugyan <sugi1982@gmail.com>
2010-03-10 17:46:04 +0900

    initial commit

---
+++ lib/App/Prove/Plugin/DesktopNotification.pm
@@ -0,0 +1,32 @@
+package App::Prove::Plugin::DesktopNotification;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Prove::Plugin::DesktopNotification -
+
+=head1 SYNOPSIS
+
+  use App::Prove::Plugin::DesktopNotification;
+
+=head1 DESCRIPTION
+
+App::Prove::Plugin::DesktopNotification is
+
+=head1 AUTHOR
+
+sugyan E<lt>sugi1982@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
